58a95e86aa13cf9fead5fbdbc82585e7612eca04
vmthuan <vmthuan16052003@gmail.com>
2023-11-09 10:37:26 +0700

    update connect firebas done

--- server/server.py
+++ server/server.py
@@ -3,8 +3,25 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-
+import json
+import firebase_admin
+from firebase_admin import credentials,db
 app = FastAPI()
+
+# For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebaseConfig = {
+    "apiKey": "AIzaSyB34_KNcAW5KI2JrcU7_4F_OEpctzn_yWw",
+    "authDomain": "firebas-htn.firebaseapp.com",
+    "databaseURL": "https://firebas-htn-default-rtdb.firebaseio.com",
+    "projectId": "firebas-htn",
+    "storageBucket": "firebas-htn.appspot.com",
+    "messagingSenderId": "640676408163",
+    "appId": "1:640676408163:web:5848a87b09350557578156",
+    "measurementId": "G-1VLQBH5G6T"
+}
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,firebaseConfig)
 
 app.add_middleware(
     CORSMiddleware,
@@ -31,6 +48,9 @@ class UserCreate(BaseModel):
 
 @app.get("/")
 def read_root():
+    ref = firebase_admin.db.reference("test/")
+    print(ref.get())
+    # check connect firebase done
     return "this is Page Home in Server"
 
 @app.post("/create_user")
